779250ef3da22741bbb5e00099e0ca49a191f4fc
HananJackie <hanan.jackie.israelevich@gmail.com>
2026-01-11 20:55:02 +0200

    Update customer_order_service.py
    now can update customer orders

--- service/customer_order_service.py
+++ service/customer_order_service.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException
+from starlette import status
+
 from model.customer import Customer
 from model.customer_order import CustomerOrder
 from model.customer_order_request import CustomerOrderRequest
@@ -34,8 +37,32 @@ async def create_customer_order(customer_order_request: CustomerOrderRequest) ->
 
 
 async def update_customer_order(customer_order_id, customer_order_request):
-    return None
+    existing_order_record = await customer_order_repository.get_by_id(customer_order_id)
+    if not existing_order_record:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"CustomerOrder with id {customer_order_id} does not exist."
+        )
+    existing_order = CustomerOrder(**dict(existing_order_record))
+    updated_order_data = customer_order_request.customer_order
+    existing_order.item_name = updated_order_data.item_name
+    existing_order.price = updated_order_data.price
+    existing_order.customer_id = existing_order.customer_id
 
+    await customer_order_repository.update_customer_order(customer_order_id, existing_order)
+
+    customer_id = existing_order.customer_id
+    customer_record = await customer_service.get_by_id(customer_id)
+    if not customer_record:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Customer with id {customer_id} not found."
+        )
+    customer = Customer(**dict(customer_record))  # <-- fix here
+    raw_orders = await customer_order_repository.get_by_customer_id(customer_id)
+    customer_orders = [CustomerOrder(**dict(r)) for r in raw_orders]
+
+    return CustomerOrderResponse(customer=customer, customer_orders=customer_orders)
 
 async def delete_by_id(customer_order_id):
     return await customer_order_repository.delete_by_id(customer_order_id)
